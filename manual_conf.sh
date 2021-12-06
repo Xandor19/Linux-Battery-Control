14ff3c1b46020573d2f038d9cd6ad2e8845b765c
Xandor19 <77125505+Xandor19@users.noreply.github.com>
2021-12-05 22:35:18 -0500

    Fixed path to file

--- manual_conf.sh
+++ manual_conf.sh
@@ -1,1 +1,1 @@
-python3 /home/xandor19/Scripts/Battery/get_env.py | sudo -S su && python3 /home/xandor19/Scripts/Battery/interface.py | sudo tee /sys/class/power_supply/BAT0/charge_control_end_threshold
+python3 "path to"/get_env.py | sudo -S su && python3 "path to"/interface.py | sudo tee /sys/class/power_supply/BAT0/charge_control_end_threshold
